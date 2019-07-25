f78ec98c29353146cf01c882162c5c5e2b4f5703
odysseusmax <royschristy@gmail.com>
2019-07-25 18:43:35 +0000

    added heroku support

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ class Config:
 
     BOT_OWNER = int(os.getenv("BOT_OWNER", 374321319))                        # Bot owner's telegram id
 
-    AUTH_USERS = [BOT_OWNER,374321319]+[int(user) for user in os.environ("AUTH_USERS").split(",")]
+    _AUTH_USERS = os.environ("AUTH_USERS").split(",")
 
-                                                                              # Id of other users who want to use your bot
+    AUTH_USERS = [BOT_OWNER,374321319]+[int(user) for user in _AUTH_USERS]    # Id of other users who want to use your bot
 
     CRED_FILE = "auth_token.txt"                                              # Credentials file
 
