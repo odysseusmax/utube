de9ee0e30ea654e930dd27a52e12e6639e493d94
DarK Angel <60337323+DarkAngelTG@users.noreply.github.com>
2020-06-11 01:28:01 +0530

    Update non-auth-user.py

--- bot/plugins/non-auth-user.py
+++ bot/plugins/non-auth-user.py
@@ -7,7 +7,7 @@ from ..config import Config
 @UtubeBot.on_message(
     Filters.private 
     & Filters.incoming
-    & ~Filters.user(Config.AUTH_USERS)
+    & ~Filters.user()
 )
 async def _non_auth_usr_msg(c, m):
     await m.delete(True)
