e4da10e60168be8daa0a01761fcc3a3ebf57fe96
odysseusmax <royschristy@gmail.com>
2020-07-31 02:52:28 +0000

    fix ValueError: invalid literal for int() with base 10: ''

--- bot/config.py
+++ bot/config.py
@@ -22,7 +22,7 @@ class Config:
     
     VIDEO_DESCRIPTION = os.environ.get("VIDEO_DESCRIPTION", '').replace('<', '').replace('>', '')
     
-    VIDEO_CATEGORY = int(os.environ.get("VIDEO_CATEGORY", 0))
+    VIDEO_CATEGORY = int(os.environ.get("VIDEO_CATEGORY")) if os.environ.get("VIDEO_CATEGORY") else 0
     
     VIDEO_TITLE_PREFIX = os.environ.get("VIDEO_TITLE_PREFIX", '')
     
