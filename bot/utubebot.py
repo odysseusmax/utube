5ba903a06102cb7d0883c7c2da2df2e13f536d08
sunilsunilma <104356829+sunilsunilma@users.noreply.github.com>
2022-04-25 19:31:13 +0530

    Update utubebot.py

--- bot/utubebot.py
+++ bot/utubebot.py
@@ -6,7 +6,6 @@ from .config import Config
 class UtubeBot(Client):
     def __init__(self):
         super().__init__(
-            session_name=Config.SESSION_NAME,
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
