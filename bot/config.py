36fa802b56522ff3f6d825da2afde6f36c478a75
sunilsunilma <104356829+sunilsunilma@users.noreply.github.com>
2022-04-26 07:05:31 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,7 +5,7 @@ class Config:
 
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
+    NAME = os.environ.get("NAME", ":memory:")
 
     API_ID = int(os.environ.get("API_ID"))
 
